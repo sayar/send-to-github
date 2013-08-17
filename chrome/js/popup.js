c4a9d3c5115c8df772ebfd6bb4d489224142d808
Rami Sayar <rami.sayar@gmail.com>
2013-08-17 16:08:52 -0400

    Loading info from selected repository.

--- chrome/js/popup.js
+++ chrome/js/popup.js
@@ -4,7 +4,45 @@ $(function(){
             items.hasOwnProperty("credentials_pass")){
             $("#login_form").addClass("hide");
             $("#issue_form").removeClass("hide");
+            
+            var github = new Github({
+                "username": items.credentials_user,
+                "password": items.credentials_pass,
+                "auth": "basic"
+            });
+            var user = github.getUser();
+            user.repos(function(err, repos){
+                for(var i in repos){
+                    var repo = repos[i];
+                    $("#issue_repository").append(new Option(repo.full_name, 
+                        repo.full_name, false, false));
+                }
+            });
+            $("#issue_repository").change(function(e){
+                $("#issue_repository option:selected").each(function () {
+                    var repo_name = $(this).val();
+                    if(repo_name !== "-"){
+                        var repo = github.getRepo(repo_name.split("/", 2)[0], 
+                            repo_name.split("/", 2)[1]);
 
+                        repo.listAssignees(function(err, assignees){
+                            var assi = [];
+                            for(var i in assignees){
+                                assi.push(assignees[i].login);
+                            }
+                            console.log(assi);
+                            $('#issue_assignee').typeahead({source:assi});
+                        });
+                        repo.listMilestones(function(err, milestones){
+                            for(var i in milestones){
+                                var mile = milestones[i];
+                                $("#issue_milestone").append(new Option(mile.title, 
+                                    mile.title, false, false));
+                            }
+                        });
+                    }
+                });
+            });
 
             $("#issue_create").click(function(event){
                 event.preventDefault();
@@ -28,19 +66,4 @@ $(function(){
             });
         }
     });
-
-    var saveChanges = function() {
-        console.log($("#issue_project"));
-        console.log($("#issue_assignee"));
-        console.log($("#issue_milestone"));
-        console.log($("#issue_labels"));
-        
-        // Save it using the Chrome extension storage API.
-//        chrome.storage.sync.set({'value': theValue}, function() {
-            // Notify that we saved.
-  //          message('Settings saved');
-    //    });
-    }
-
-
 });
